3fddd0c4b9cb4de6142174c2276384f35a5d399a
Nicholas Corder <niccorder25@gmail.com>
2018-07-01 00:23:06 -0700

    Setup activity to display news grid.

--- app/src/main/java/me/niccorder/fakedata/FakeNewsActivity.java
+++ app/src/main/java/me/niccorder/fakedata/FakeNewsActivity.java
@@ -1,22 +1,55 @@
 package me.niccorder.fakedata;
 
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.ImageView;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.resource.bitmap.CircleCrop;
-import com.bumptech.glide.request.Request;
-import com.bumptech.glide.request.RequestOptions;
-
-import jp.wasabeef.glide.transformations.RoundedCornersTransformation;
+import me.niccorder.fakedata.data.NewsRepository;
 
 public class FakeNewsActivity extends AppCompatActivity {
+
+    /**
+     * The news repository provides news articles.
+     */
+    private NewsRepository newsRepository;
+
+    /**
+     * A swipe refresh layout allows PTR (pull to refresh) on scrollable views.
+     */
+    private SwipeRefreshLayout refreshLayout;
+
+    private RecyclerView newsRecycler;
+    private StaggeredGridLayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fake_news);
 
+        // Grab a reference to our news repository
+        newsRepository = ((FakeNewsApplication) getApplication()).getNewsRepository();
+
+        // Grab a reference to our views.
+        refreshLayout = findViewById(R.id.refresh_container);
+        newsRecycler = findViewById(R.id.recycler);
+
+        // Sets the color wheel colors shown during a PTR (pull to refresh).
+        refreshLayout.setColorSchemeResources(
+                R.color.colorPrimary,
+                R.color.colorAccent,
+                R.color.colorPrimaryDark
+        );
+
+        // Init the layout manager & attach to recycler.
+        layoutManager = new StaggeredGridLayoutManager(
+                2,
+                StaggeredGridLayoutManager.VERTICAL
+        );
+        layoutManager.setGapStrategy(
+                StaggeredGridLayoutManager.GAP_HANDLING_MOVE_ITEMS_BETWEEN_SPANS
+        );
+        newsRecycler.setLayoutManager(layoutManager);
     }
 }
